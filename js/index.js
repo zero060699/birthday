256eb90beb0f1bf26435e0b946e483f543ed9151
LocBQ <locbq@bkav.com>
2023-10-25 09:44:44 +0700

    fix-link-youtube

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/watch?v=n3qw7lZzbkM");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/n3qw7lZzbkM?si=OHpxJTQYNHSlnQu9?controls=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
